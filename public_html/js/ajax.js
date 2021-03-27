97ff02d455c5a447c6fa05deec8bd88d7684c344
Teh Boon Hock <tehboonhock@hotmail.com>
2021-03-28 01:47:47 +0800

    solved ajax not working in github.io have to allow Access-Control-Allow-Origin in htaccess

--- public_html/js/ajax.js
+++ public_html/js/ajax.js
@@ -10,8 +10,6 @@ function doAjax(getPost, url, data, doBefore, doSuccess) {
 		url = "https://incupedev.000webhostapp.com/" + url
 	}
 
-	console.log(url);
-
 	xhr = $.ajax({
 		type: getPost,
 		url: url,
