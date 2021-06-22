98f1a15222e3060198449e20095d3b25718fb461
Teh Boon Hock <tehboonhock@hotmail.com>
2021-06-22 23:47:09 +0800

    deleted and re-create another 000webhost website. old one load very slow. dunno why

--- development/php/conn.php
+++ development/php/conn.php
@@ -1,17 +1,13 @@
 <?php
 
 $servername = "localhost";
-$db = "id16429686_yromem_db";
-$username = "id16429686_yromem_admin";
+$db = "id17109168_yromem_db";
+$username = "id17109168_yromem_admin";
 $pw = "aeaLKT(]l%NbLH0X";
-// $db = "id16429686_yromem_db";
-// $username = "id16429686_yromem_admin";
-// $pw = "aeaLKT(]l%NbLH0X";
-// $port = "3306";
+$port = "3306"; // 000webhost using 3306. localhost using 3308
 
 try {
-    // $conn = new PDO("mysql:host=$servername;dbname=$db;port=$port;charset=utf8;", $username, $pw);
-    $conn = new PDO("mysql:host=$servername;dbname=$db;charset=utf8;", $username, $pw);
+    $conn = new PDO("mysql:host=$servername;dbname=$db;port=$port;charset=utf8;", $username, $pw);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
